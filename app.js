ea747003c84a9f66a5fa90703b300c9b2404422b
Jian <jian.media@gmail.com>
2024-05-29 13:51:01 +0100

    task 4 completed

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express")
 
-const {getTopics, getApi} = require('./controllers/api_controller')
+const {getTopics, getApi, getArticleById} = require('./controllers/api_controller')
 
 const app = express();
 
 app.get('/api/topics', getTopics)
 
 app.get('/api', getApi)
+
+app.get('/api/articles/:article_id', getArticleById)
 
 app.all('*', (req, res) => {
     res.status(404).send({msg: 'endpoint does not exist'})
